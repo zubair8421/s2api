ed1efb66cde07582bded38d6a5142715dbd7e95c
zubair8421 <75317575+zubair8421@users.noreply.github.com>
2020-12-11 18:18:07 +0500

    s2 commit

---
+++ App.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const PORT = 4000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://zubair:Dhaphase6125@cluster0.t5cvz.mongodb.net/test",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("Connected to mongoDB.."))
+  .catch((err) => console.error("Could not connect..", err));
+
+const courseSchema = new mongoose.Schema({
+  crName: String,
+  id: Number,
+  crDuration: Number,
+  crFee: Number,
+});
+
+const Courses = mongoose.model("Courses", courseSchema);
+
+app.post("/api/getCourses", (req, res) => {
+  var newEntry = new Courses({
+    crName: req.body.crName,
+    id: req.body._id,
+    crDuration: req.body.crDuration,
+    crFee: req.body.crFee,
+  });
+  newEntry
+    .save()
+    .then((result) => {
+      res.status(200).json({ success: true, document: result });
+    })
+    .catch((err) => console.log(err));
+});
+
+app.put("/api/deleteCourse/:_id", (req, res) => {
+  Courses.findByIdAndDelete({ _id: req.params._id }, (err, data) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(data);
+    }
+  });
+});
+app.put("/api/updateCourse/:_id", (req, res) => {
+  Courses.findByIdAndUpdate(
+    { _id: req.params._id },
+    {
+      crName: req.body.crName,
+      id: req.body.id,
+      crDuration: req.body.crDuration,
+      crFee: req.body.crFee,
+    },
+    (err, data) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(data);
+      }
+    }
+  );
+});
+
+app.listen(PORT, function () {
+  console.log("Server is running on Port: " + PORT);
+});
